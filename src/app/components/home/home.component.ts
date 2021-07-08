15664e0de3e9489d46240f93aa3331ca5018404a
Divya <singhdivyasingh17@gmail.com>
2021-07-08 16:25:41 +0530

    commiting story modal changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { StoryComponent } from '../story/story.component';
 
@@ -7,17 +6,17 @@ export interface Tile {
   status: string;
 }
 export enum Status {
-  TO_Do = "To Do",
-  IN_PROGRESS = "In Progress",
-  RESOLVED = "Resolved",
-  CLOSED = "Closed",
+  TO_Do = 'To Do',
+  IN_PROGRESS = 'In Progress',
+  RESOLVED = 'Resolved',
+  CLOSED = 'Closed',
 }
 export interface Story {
-  storyId : string;
-  heading : string;
-  assignee : string;
-  storyPoints : number;
-  status : Status;
+  storyId: string;
+  heading: string;
+  assignee: string;
+  storyPoints: number;
+  status: Status;
 }
 
 @Component({
@@ -33,30 +32,74 @@ export class HomeComponent implements OnInit {
     { status: Status.CLOSED },
   ];
   stories: Story[] = [
-    {storyId: "Story 1",heading: "New story",assignee:"Mohan",storyPoints:4 ,status:Status.TO_Do },
-    {storyId: "Story 2",heading: "Progress story",assignee:"Sohan",storyPoints:3 ,status:Status.IN_PROGRESS },
-    {storyId: "Story 3",heading: "Resolved story",assignee:"Rohan",storyPoints:5 ,status:Status.RESOLVED },
-    {storyId: "Story 4",heading: "Closed story",assignee:"Kohan",storyPoints:6 ,status:Status.CLOSED },
-    {storyId: "Story 5",heading: "New story",assignee:"Kohan",storyPoints:4 ,status:Status.TO_Do },
-    {storyId: "Story 6",heading: "Progess story",assignee:"Mohan",storyPoints:5 ,status:Status.IN_PROGRESS },
-    {storyId: "Story 7",heading: "Resolved story",assignee:"Rohan",storyPoints:6 ,status:Status.RESOLVED },
-    {storyId: "Story 8",heading: "Closed story",assignee:"Sohan",storyPoints:3 ,status:Status.CLOSED },
-
+    {
+      storyId: 'Story 1',
+      heading: 'New story',
+      assignee: 'Mohan',
+      storyPoints: 4,
+      status: Status.TO_Do,
+    },
+    {
+      storyId: 'Story 2',
+      heading: 'Progress story',
+      assignee: 'Sohan',
+      storyPoints: 3,
+      status: Status.IN_PROGRESS,
+    },
+    {
+      storyId: 'Story 3',
+      heading: 'Resolved story',
+      assignee: 'Rohan',
+      storyPoints: 5,
+      status: Status.RESOLVED,
+    },
+    {
+      storyId: 'Story 4',
+      heading: 'Closed story',
+      assignee: 'Kohan',
+      storyPoints: 6,
+      status: Status.CLOSED,
+    },
+    {
+      storyId: 'Story 5',
+      heading: 'New story',
+      assignee: 'Kohan',
+      storyPoints: 4,
+      status: Status.TO_Do,
+    },
+    {
+      storyId: 'Story 6',
+      heading: 'Progess story',
+      assignee: 'Mohan',
+      storyPoints: 5,
+      status: Status.IN_PROGRESS,
+    },
+    {
+      storyId: 'Story 7',
+      heading: 'Resolved story',
+      assignee: 'Rohan',
+      storyPoints: 6,
+      status: Status.RESOLVED,
+    },
+    {
+      storyId: 'Story 8',
+      heading: 'Closed story',
+      assignee: 'Sohan',
+      storyPoints: 3,
+      status: Status.CLOSED,
+    },
   ];
 
+  constructor(public dialog: MatDialog) {}
 
-  constructor(private router : Router ,public dialog: MatDialog) {}
-
-  openStoryDetails(){
-    //this.router.navigate(['story']);
+  openStoryDetails(index: number) {
     const dialogRef = this.dialog.open(StoryComponent, {
       width: '250px',
-      data: { story: this.stories[0].storyId , heading: this.stories[0].heading }
+      data: this.stories[index],
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed', result);
-
     });
   }
 
--- src/app/components/story/story.component.ts
+++ src/app/components/story/story.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Story } from '../home/home.component';
 
 @Component({
   selector: 'app-story',
   templateUrl: './story.component.html',
-  styleUrls: ['./story.component.scss']
+  styleUrls: ['./story.component.scss'],
 })
 export class StoryComponent implements OnInit {
-
-  constructor() { }
+  storymap = new Map<string, string>();
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Story) {}
 
   ngOnInit(): void {
-  }
+    console.log(Object.keys(this.data));
 
+    Object.keys(this.data).forEach((key) => {
+      this.storymap.set(key, this.data[key]);
+    });
+  }
 }
